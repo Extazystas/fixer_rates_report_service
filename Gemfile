c917d148c4061268527f92070e2343a0a9a7689b
extazystas <extazystas@gmail.com>
2020-12-22 21:27:29 +0300

    Add progressbar gem for progress visualization

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'faraday'
+# Text progress bar for terminal
+gem 'progressbar'
 gem 'pry'
 gem 'rspec'
 # Static ruby code analyzer (linter)
--- execute.rb
+++ execute.rb
@@ -1,12 +1,14 @@
 begin
   require 'pry'
   require 'csv'
+  require 'progressbar'
   require 'rspec'
 rescue LoadError => e
   puts "#{e}. Installing..."
   # Installing missed gems
   system('gem install pry')
   system('gem install rspec')
+  system('gem install progressbar')
   puts 'Installation was completed successfully. Please re-run the program'
   exit
 end
