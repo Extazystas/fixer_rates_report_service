e9b09969f6f923a24bfe8017d24380def17e28b2
extazystas <extazystas@gmail.com>
2020-12-23 16:05:49 +0300

    Add progress bar to ReportsGenerator

--- execute.rb
+++ execute.rb
@@ -7,21 +7,22 @@ end
 begin
   require 'anyway_config'
   require 'pry'
-  require 'progressbar'
+  require 'ruby-progressbar'
   require 'rspec'
 rescue LoadError => e
   puts "#{e}. Installing..."
   # Installing missed gems
   system('gem install pry')
   system('gem install rspec')
-  system('gem install progressbar')
   system('gem install anyway_config')
+  system('gem install ruby-progressbar')
   puts 'Installation was completed successfully. Please re-run the program'
   exit
 end
 
 Anyway::Settings.default_config_path = ->(name) { "#{BASE_PATH}/config/settings/#{name}.yml" }
 
+# Report generation workflow:
 rates_info = RatesFetcher.call
 ReportsGenerator.new(rates_info).call
 ReportsUploader.new.call if ENV['RACK_ENV'] == 'production'
--- lib/services/reports_generator.rb
+++ lib/services/reports_generator.rb
@@ -1,3 +1,5 @@
+require 'ruby-progressbar'
+
 class ReportsGenerator
   attr_reader :report_formats, :rates_info
 
@@ -7,8 +9,30 @@ class ReportsGenerator
   end
 
   def call
-    report_formats.each do |format|
-      Module.const_get("::Formatters::#{format.capitalize}Formatter").new(rates_info)
-    end
+    progress_bar.log("Starting report generation for #{report_formats.join(', ')} formats.")
+
+    report_formats.each { |format| generate_report(format) }
+
+    progress_bar.log('All reports successfully generated!')
+  end
+
+  private
+
+  def generate_report(format)
+    progress_bar.log("Generating #{format} report...")
+
+    Module.const_get("::Formatters::#{format.capitalize}Formatter").new(rates_info)
+
+    progress_bar.increment
+  end
+
+  def progress_bar
+    @progress_bar ||= ProgressBar.create(
+      format: "%a %b\u{15E7}%i %c/%C %p%% %e",
+      progress_mark: ' ',
+      remainder_mark: "\u{FF65}",
+      starting_at: 0,
+      total: report_formats.size
+    )
   end
 end
